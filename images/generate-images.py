6e0c09427b41cfe261f90c5a1342d9f46cd83069
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2022-07-27 14:14:42 +0300

    Generate dark and light images simultaneously

--- images/generate-images.py
+++ images/generate-images.py
@@ -2,13 +2,20 @@
 
 from os import system, listdir
 
-exclude_tex_files = ['main.tex', 'tmp.tex']
+exclude_tex_files = ['main.tex', 'main_dark.tex', 'tmp.tex']
 
 for file_name in listdir('.'):
     if file_name not in exclude_tex_files and file_name.endswith('.tex'):
         system(f'cp {file_name} tmp.tex')
-        system('pdflatex main')
+        print(f'Processing {file_name[:-4]}')
+
+        # light picture
+        system('pdflatex main > /dev/null 2>&1')
         system(f'convert -density 1000 main.pdf {file_name[:-4]}.png')
 
-system('rm *.log *.aux main.pdf tmp.pdf tmp.png')
+        # uncomment this later, when Idea is able to process two types of images
+        # # dark picture
+        # system('pdflatex main_dark > /dev/null 2>&1')
+        # system(f'convert -density 1000 main_dark.pdf {file_name[:-4]}_dark.png')
 
+system('rm *.log *.aux main.pdf tmp.pdf tmp.png')
