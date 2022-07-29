f58b9798aeacf8e4de5962809c0c76a992fb050d
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2022-07-29 19:10:00 +0300

    solutions extracted to a separate file

---
+++ Divide and Conquer/Opposite Values/solution.md
@@ -0,0 +1,61 @@
+
+### Solution
+
+Consider a 0/1-array where the first element is 0
+and the last element is 1.
+An element 0 followed by an element 1 in this array is called
+a *0-1 pair*. Here is an example of an array with six 0-1 pairs.
+Here is an example of an array with six white-black pairs.
+
+![](../../images/pair01.png)
+
+However, you don’t see the elements
+(except for the first and the last one), for you, this array looks like this:
+
+![](../../images/pair01_2.png)
+
+You may point to any cell and ask a question
+“What is this element?” Your goal is to find a 0-1 pair by asking
+the minimum number of questions.
+[Try the puzzle!](https://discrete-math-puzzles.github.io/puzzles/opposite-colors/index.html)
+After solving this puzzle, you will see that the binary search is
+useful even when the data is not sorted!
+
+But how do we know that there is a 0-1 pair?
+This is true because when moving from the first 0 to the last 1,
+the value of a cell should eventually switch from 0 to 1 at least once.
+
+Even though this proof works for any array that starts from 0 and ends in 1,
+it is *non-constructive*: it proves that there exists a 0-1 pair, but doesn't
+give an *efficient method* for finding this pair
+(by revealing the value of a few cells).
+In particular, if we start checking the value of the cells one by one from the left,
+then we will eventually find a 0-1 pair, but in the worst case it will
+require us to reveal the values of all cells.
+
+Thus, we know that a 0-1 pair exists, but we still need to figure out an
+efficient method for finding it. Inspired by our previous examples, let’s reveal
+the value of the middle cell. Assume, for example, that it is 0.
+
+![](../../images/pair01_3.png)
+
+**Stop and Think.** If the middle cell turned out to be 0, how would you proceed?
+
+At first sight, it does not help us much.
+If we start revealing the values of its neighbor cells,
+both of them may be 0's. Instead, let’s focus on the right part of the array.
+
+![](../../images/pair01_4.png)
+
+Do you see? It is the same problem again! Since the leftmost cell is 0,
+the rightmost one is 1, it must contain a white-black pair. And its length is twice smaller.
+If the middle cell turns out to be 1, we can also find a subarray that
+starts in 0 and ends in 1 (though of length 11, but not 10).
+
+![](../../images/pair01_5.png)
+
+Hence, in any case, we decrease the size by a factor of (almost) two: from
+a starting array of length $n$, we get an array of length $\lceil \frac{n+1}{2} \rceil$.
+Thus, after revealing the values of at most five cells, we will find a 0-1 pair.
+Indeed, even in the worst possible case, the size of the array will shrink as follows:
+$20 \to 11 \to 6 \to 4 \to 3 \to 2$.
--- buildSrc/src/main/groovy/ResolveTemplate.java
+++ buildSrc/src/main/groovy/ResolveTemplate.java
@@ -2,34 +2,91 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.HashSet;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ResolveTemplate {
+    private final File currentDirectory;
+    private final File rootDirectory;
+    private final File currentFile;
+    private final HashSet<String> includedPathsSoFar;
+
+    public ResolveTemplate(File currentFile, File rootDirectory, HashSet<String> includedPathsSoFar) throws IOException {
+        this.currentFile = currentFile;
+        this.currentDirectory = currentFile.getParentFile();
+        this.rootDirectory = rootDirectory;
+        this.includedPathsSoFar = new HashSet<>(includedPathsSoFar);
+        this.includedPathsSoFar.add(currentFile.getCanonicalPath());
+    }
+
+    public ResolveTemplate(File currentFile, File rootDirectory) throws IOException {
+        this(currentFile, rootDirectory, new HashSet<>());
+    }
+
 
     private static String readFileContent(File file) throws IOException {
         return String.join("\n", Files.readAllLines(file.toPath(), StandardCharsets.UTF_8));
     }
 
-    private static String includeCss(String content, File projectDirectory) throws IOException {
+    private File resolvePath(String filePath) {
+        boolean absolute = false;
+        while (filePath.startsWith("/") || filePath.startsWith(File.separator)) {
+            filePath = filePath.substring(1);
+            absolute = true;
+        }
+        if (absolute) {
+            return new File(rootDirectory, filePath);
+        } else {
+            return new File(currentDirectory, filePath);
+        }
+    }
+
+    private static Matcher findSubstitute(String name, String content) {
+        Pattern pattern = Pattern.compile("<%" + name + "\\s+\"([^\"]+)\"\\s+%>");
+        return pattern.matcher(content);
+    }
+
+    private String substitute(String name, String content, Function<String, String> transformer) throws IOException {
         while (true) {
-            Pattern pattern = Pattern.compile("<%css\\s+\"([^\"]+)\"\\s+%>");
-            Matcher matcher = pattern.matcher(content);
+            Matcher matcher = findSubstitute(name, content);
             if (!matcher.find()) {
                 break;
             }
             String cssFileName = matcher.group(1);
-            String cssContent = readFileContent(new File(projectDirectory, cssFileName));
+            File cssFile = resolvePath(cssFileName);
+            String cssContent;
+            if (includedPathsSoFar.contains(cssFile.getCanonicalPath())) {
+                cssContent = readFileContent(cssFile);
+            } else {
+                ResolveTemplate resolver = new ResolveTemplate(cssFile, rootDirectory, includedPathsSoFar);
+                cssContent = resolver.resolve();
+            }
             content = content.substring(0, matcher.start()) +
-                    String.format("<style>\n%s\n</style>", cssContent) +
+                    transformer.apply(cssContent) +
                     content.substring(matcher.end());
         }
         return content;
     }
 
-    static void resolveTemplate(File template, File target, File projectDirectory) throws IOException {
-        String content = readFileContent(template);
-        content = includeCss(content, projectDirectory);
-        Files.writeString(target.toPath(), content, StandardCharsets.UTF_8);
+    private String includeCss(String content) throws IOException {
+        return substitute("css", content, cssContent -> String.format("<style>\n%s\n</style>", cssContent));
+    }
+
+    private String include(String content) throws IOException {
+        return substitute("include", content, Function.identity());
+    }
+
+    private String resolve() throws IOException {
+        String content = readFileContent(currentFile);
+        content = includeCss(content);
+        content = include(content);
+        return content;
+    }
+
+    static void resolveTemplate(File template, File target, File rootDirectory) throws IOException {
+        ResolveTemplate resolver = new ResolveTemplate(template, rootDirectory);
+        Files.writeString(target.toPath(), resolver.resolve(), StandardCharsets.UTF_8);
     }
 }
