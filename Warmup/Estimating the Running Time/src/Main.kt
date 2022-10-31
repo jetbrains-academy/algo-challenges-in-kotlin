b0a0dfd134a728637e271dfe0ad3dc19f7aa24fc
Niyaz Nigmatullin <niyaznigmatullin@github.com>
2022-10-31 12:18:10 +0200

    Estimating the Running Time: add functions with different running times

--- Warmup/Estimating the Running Time/src/Main.kt
+++ Warmup/Estimating the Running Time/src/Main.kt
@@ -1,3 +1,5 @@
+import kotlin.system.measureTimeMillis
+
 /*
 Niyaz, let's give three methods here — with running time, say,
 n, n^2, 2^n (each of them may just loop for the corresponding
@@ -7,7 +9,47 @@ time. We will then invite the learners to fool around with the values
 of n to see how the running time changes.
  */
 
+fun linear(a: IntArray): Int {
+    var count = 0
+    for (x in a) {
+        if (x > 0) {
+            count++
+        }
+    }
+    return count
+}
+
+fun quadratic(a: IntArray): Int {
+    var count = 0
+    for (i in a.indices) {
+        for (j in a.indices.drop(i + 1)) {
+            if (a[i] + a[j] > 0) {
+                count++
+            }
+        }
+    }
+    return count
+}
+
+fun exponential(a: IntArray): Long {
+    fun backtrack(index: Int, sum: Long): Long {
+        if (index == a.size) {
+            return when {
+                sum > 0 -> 1
+                else -> 0
+            }
+        }
+        return backtrack(index + 1, sum) + backtrack(index + 1, sum + a[index])
+    }
+    return backtrack(0, 0L)
+}
+
 
 fun main() {
-    // Write your solution here
+    val n = 30
+    val a = IntArray(n) { 1 }
+    val time = measureTimeMillis {
+        println(exponential(a))
+    }
+    println("Took $time ms")
 }
