0169f6c3f62da868daa0a0734a99ecc2ccf463e7
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2023-06-01 23:40:20 +0300

    Maximum Pairwise - First issue: add solution and sample tests

--- Warmup/Maximum Pairwise Product — Fixing the First Issue/src/Task.kt
+++ Warmup/Maximum Pairwise Product — Fixing the First Issue/src/Task.kt
@@ -1,1 +1,12 @@
-// type your solution here
+import kotlin.math.max
+
+fun maximumPairwiseProduct(a: IntArray): Long {
+    val len = a.size
+    var res = Long.MIN_VALUE
+    for (i in 0 until len) {
+        for (j in i + 1 until len) {
+            res = max(res, a[i].toLong() * a[j])
+        }
+    }
+    return res
+}
--- Warmup/Maximum Pairwise Product — Fixing the First Issue/test/Tests.kt
+++ Warmup/Maximum Pairwise Product — Fixing the First Issue/test/Tests.kt
@@ -1,9 +1,29 @@
-import org.junit.Assert
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
-class Test {
-    @Test fun testSolution() {
-        //TODO: implement your test here
-        Assert.assertTrue("Tests not implemented for the task", false)
+class Tests {
+    @Test
+    fun sample1() {
+        val a = intArrayOf(1, 2, 3)
+        val correct = 6L
+        val result = maximumPairwiseProduct(a)
+        assertEquals(result, correct, "maximumPairwiseProduct(${a.contentToString()})")
     }
+
+    @Test
+    fun sample2() {
+        val a = intArrayOf(7, 5, 14, 2, 8, 8, 10, 1, 2, 3)
+        val correct = 140L
+        val result = maximumPairwiseProduct(a)
+        assertEquals(result, correct, "maximumPairwiseProduct(${a.contentToString()})")
+    }
+
+    @Test
+    fun sample3() {
+        val a = intArrayOf(1_000_000, 1_000_000)
+        val correct = 1_000_000_000_000L
+        val result = maximumPairwiseProduct(a)
+        assertEquals(correct, result, "maximumPairwiseProduct(${a.contentToString()}")
+    }
+
 }
