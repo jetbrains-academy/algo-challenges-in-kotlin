2fa67a46fd2e2d478c83d6cb0c6c73a1830ccd6a
pmavrin <pavel.mavrin@gmail.com>
2023-06-01 16:32:00 +0300

    Added Kotlin code

--- Divide and Conquer/Searching Sorted Data/src/Main.kt
+++ Divide and Conquer/Searching Sorted Data/src/Main.kt
@@ -1,3 +1,15 @@
-fun main() {
-    // Write your solution here
+fun find(a: IntArray, x: Int): Int {
+    var lower = 0
+    var upper = a.size - 1
+    while (upper >= lower) {
+        val middle = (lower + upper) / 2
+        if (a[middle] == x) {
+            return middle
+        } else if (a[middle] > x) {
+            upper = middle - 1
+        } else {
+            lower = middle + 1
+        }
+    }
+    return -1
 }
