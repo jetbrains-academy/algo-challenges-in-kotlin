214c38c7d2451b723bcd8ffefc467127c1f431f5
pmavrin <pavel.mavrin@gmail.com>
2023-06-01 16:08:31 +0300

    Added solution and example test

--- Warmup/Maximum Pairwise Product/src/Task.kt
+++ Warmup/Maximum Pairwise Product/src/Task.kt
@@ -1,1 +1,13 @@
-// type your solution here
+import kotlin.math.max
+
+fun maximumPairwiseProduct(a: IntArray): Int {
+    val len = a.size
+    var res = Int.MIN_VALUE
+    for (i in 0 until len) {
+        for (j in i + 1 until len) {
+            res = max(res, a[i] * a[j])
+        }
+    }
+    return res
+}
+
--- Warmup/Maximum Pairwise Product/test/Tests.kt
+++ Warmup/Maximum Pairwise Product/test/Tests.kt
@@ -1,9 +1,21 @@
-import org.junit.Assert
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
-class Test {
-    @Test fun testSolution() {
-        //TODO: implement your test here
-        Assert.assertTrue("Tests not implemented for the task", false)
+class Tests {
+    @Test
+    fun sample1() {
+        val a = intArrayOf(1, 2, 3)
+        val correct = 6
+        val result = maximumPairwiseProduct(a)
+        assertEquals(result, correct, "maximumPairwiseProduct($a)")
     }
+
+    @Test
+    fun sample2() {
+        val a = intArrayOf(7, 5, 14, 2, 8, 8, 10, 1, 2, 3)
+        val correct = 140
+        val result = maximumPairwiseProduct(a)
+        assertEquals(result, correct, "maximumPairwiseProduct($a)")
+    }
+
 }
