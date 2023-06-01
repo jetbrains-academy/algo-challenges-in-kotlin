a8fa6fec4f479a54b22c627f9b5f05d3b5008468
pmavrin <pavel.mavrin@gmail.com>
2023-06-01 16:22:54 +0300

    Added Kotlin code

--- Divide and Conquer/Guess a Number/src/Main.kt
+++ Divide and Conquer/Guess a Number/src/Main.kt
@@ -1,3 +1,22 @@
-fun main() {
-    // Write your solution here
+fun guess(lower: Int, upper: Int) {
+    val middle = (lower + upper) / 2
+    val result = query(middle)
+    if (result == '=') {
+        return
+    } else if (result == '<') {
+        guess(lower, middle - 1)
+    } else {
+        guess(middle + 1, upper)
+    }
+}
+
+val x = 4 // Change this value for testing
+fun query(y: Int): Char {
+    if (y == x) {
+        return '='
+    } else if (y < x) {
+        return '<'
+    } else {
+        return '>'
+    }
 }
