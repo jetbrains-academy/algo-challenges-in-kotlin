de39f9324e7e676e3a103ea684339cf42c00861e
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2023-06-15 18:49:12 +0300

    Typo

--- images/generate-images.py
+++ images/generate-images.py
@@ -29,6 +29,6 @@ if __name__ == '__main__':
         system('pdflatex main_dark > /dev/null 2>&1')
         system(f'convert -density 700 -colorspace RGB main_dark.pdf {file_name[:-4]}_dark.png')
         
-	print('OK')
+        print('OK')
 
     system('rm *.log *.aux main.pdf')
