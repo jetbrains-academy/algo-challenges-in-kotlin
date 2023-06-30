d6be514d4ccdb28c45fb38ceb31613ac86a4d8ab
pmavrin <pavel.mavrin@gmail.com>
2023-06-30 13:49:31 +0300

    Add hidden tests

--- Divide and Conquer/Count in Range/test/HiddenTests.kt
+++ Divide and Conquer/Count in Range/test/HiddenTests.kt
@@ -7,6 +7,7 @@ class HiddenTests {
     companion object {
         private val random = Random(228)
     }
+
     @Test
     fun example1() = runTimeout(1.seconds, "Example 1") {
         val result = countInRange(intArrayOf(2, 3, 5, 8, 10), 3, 9)
@@ -25,4 +26,67 @@ class HiddenTests {
         Assertions.assertEquals(result, 0)
     }
 
+    @Test
+    fun small_random_tests() = runTimeout(2.seconds, "small_random_tests") {
+        val TESTS_NUMBER = 100
+        val MAX_N = 100
+        val MAX_VAL = 1000
+        for (i in 1..TESTS_NUMBER) {
+            val n = random.nextInt(1, MAX_N)
+            val a = IntArray(n) { random.nextInt(0, MAX_VAL) }
+            a.sort()
+            var l = random.nextInt(0, MAX_VAL)
+            var r = random.nextInt(0, MAX_VAL)
+            if (l > r) {
+                val t = l
+                l = r
+                r = t
+            }
+            run_test(a, l, r)
+        }
+    }
+
+    @Test
+    fun large_random_tests() = runTimeout(2.seconds, "large_random_tests") {
+        val TESTS_NUMBER = 10000
+        val MAX_N = 1000000
+        val MAX_VAL = 1000000000
+        val n = MAX_N
+        val a = IntArray(n) { random.nextInt(0, MAX_VAL) }
+        a.sort()
+        for (i in 1..TESTS_NUMBER) {
+            var l = random.nextInt(0, MAX_VAL)
+            var r = random.nextInt(0, MAX_VAL)
+            if (l > r) {
+                val t = l
+                l = r
+                r = t
+            }
+            run_test(a, l, r)
+        }
+    }
+
+    private fun run_test(a: IntArray, l: Int, r: Int) {
+        val result = countInRange(a, l, r)
+        val correct = modelSolution(a, l, r)
+        Assertions.assertEquals(result, correct)
+    }
+
+    private fun modelSolution(a: IntArray, l: Int, r: Int): Int {
+        return lowerBound(a, r + 1) - lowerBound(a, l)
+    }
+
+    private fun lowerBound(a: IntArray, x: Int): Int {
+        var lower = -1
+        var upper = a.size
+        while (upper > lower + 1) {
+            val middle = (lower + upper) / 2
+            if (a[middle] < x) {
+                lower = middle
+            } else {
+                upper = middle
+            }
+        }
+        return upper
+    }
 }
