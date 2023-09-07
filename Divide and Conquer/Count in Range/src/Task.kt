19ce0cc7bef7d2078923b411c17ca0910b981306
pmavrin <pavel.mavrin@gmail.com>
2023-09-07 15:56:30 +0300

    Refactor solution in a functional style, to make it look more fancy

--- Divide and Conquer/Count in Range/src/Task.kt
+++ Divide and Conquer/Count in Range/src/Task.kt
@@ -1,17 +1,16 @@
-
 fun countInRange(a: IntArray, l: Int, r: Int): Int {
-    return lowerBound(a, r + 1) - lowerBound(a, l)
+    return lowerBound(a) { it > r } - lowerBound(a) {it >= l}
 }
 
-fun lowerBound(a: IntArray, x: Int): Int {
+fun lowerBound(a: IntArray, f: (Int) -> Boolean): Int {
     var lower = -1
     var upper = a.size
     while (upper > lower + 1) {
         val middle = (lower + upper) / 2
-        if (a[middle] < x) {
-            lower = middle
-        } else {
+        if (f(a[middle])) {
             upper = middle
+        } else {
+            lower = middle
         }
     }
     return upper
